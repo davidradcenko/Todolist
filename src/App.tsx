a67196917ce60731c3eed4975e0d35b2d2704cbd
David <davidradchenko2@gmail.com>
2022-03-10 00:29:04 +0300

    video 02-1:16

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist, TypeTask} from "./Todolist";
+import {Todolist, TaskType} from "./Todolist";
 
+export type FiltorValeosType= "all" | "completed" | "active";
 
 function App() {
-
-    let initialTasks = [
+    let [tasks, setTasks] = useState< Array<TaskType>>([
         {id: 1, title: "Css", isDone: true},
         {id: 2, title: "Js", isDone: true},
         {id: 3, title: "React", isDone: false},
-    ]
-   let arr= useState(initialTasks)
-    let tasks =arr[0];
-    let setTasks=arr[1]
+    ])
+    let [filter,setFilter] = useState<FiltorValeosType>("all")
 
+    function changeFilter(value:FiltorValeosType) {
+        setFilter(value);
+    }
     function RemoveTasks(id: number) {
-       let filterTasks = tasks.filter(t => t.id !== id)
+        let filterTasks = tasks.filter(t => t.id !== id)
         setTasks(filterTasks)
     }
 
-
+let taskForTodolist = tasks;
+    if (filter === "completed"){
+        taskForTodolist = tasks.filter(t => t.isDone === true);
+    }
+    if (filter === "active"){
+        taskForTodolist = tasks.filter(t => t.isDone === false);
+    }
     return (
         <div className="App">
             <Todolist title={"What to learn"}
-                      tasks={tasks}
-                      RemoveTasks={RemoveTasks}/>
+                      tasks={taskForTodolist}
+                      RemoveTasks={RemoveTasks}
+                      changeFilter={changeFilter}/>
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import {futimes} from "fs";
 
- export type TypeTask = {
+ export type TaskType = {
     id: number, title: string, isDone: boolean
 }
 type PropsType = {
     title: string,
-    tasks: Array<TypeTask>,
-    RemoveTasks: Function
+    tasks: Array<TaskType>,
+    RemoveTasks: Function,
+    changeFilter:Function
 }
 
 export function Todolist(props: PropsType) {
@@ -23,9 +25,9 @@ export function Todolist(props: PropsType) {
                 </li>)}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={()=>{props.changeFilter("all")}}>All</button>
+                <button onClick={()=>{props.changeFilter("active")}}>Active</button>
+                <button onClick={()=>{props.changeFilter("completed")}}>Completed</button>
             </div>
         </div>
     )
