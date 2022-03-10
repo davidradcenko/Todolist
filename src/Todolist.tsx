4552ffd1321f33c51d7f3388a94ebff55adef989
David <davidradchenko2@gmail.com>
2022-03-11 00:14:04 +0300

    video 02 alredy

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import {futimes} from "fs";
+import {FiltorValeosType} from "./App";
 
- export type TaskType = {
+export type TaskType = {
     id: number, title: string, isDone: boolean
 }
 type PropsType = {
     title: string,
     tasks: Array<TaskType>,
-    RemoveTasks: Function,
-    changeFilter:Function
+    RemoveTasks: (id: number) => void,
+    changeFilter: (value: FiltorValeosType) => void
 }
 
 export function Todolist(props: PropsType) {
@@ -19,15 +20,27 @@ export function Todolist(props: PropsType) {
                 <button>+</button>
             </div>
             <ul>
-                {props.tasks.map( t => <li><input type="checkbox" checked={t.isDone}/>
-                <span>{t.title}</span>
-                <button onClick={()=>{props.RemoveTasks(t.id)}}>X</button>
+                {props.tasks.map(t => <li><input type="checkbox" checked={t.isDone}/>
+                    <span>{t.title}</span>
+                    <button onClick={() => {
+                        props.RemoveTasks(t.id)
+                    }}>X
+                    </button>
                 </li>)}
             </ul>
             <div>
-                <button onClick={()=>{props.changeFilter("all")}}>All</button>
-                <button onClick={()=>{props.changeFilter("active")}}>Active</button>
-                <button onClick={()=>{props.changeFilter("completed")}}>Completed</button>
+                <button onClick={() => {
+                    props.changeFilter("all")
+                }}>All
+                </button>
+                <button onClick={() => {
+                    props.changeFilter("active")
+                }}>Active
+                </button>
+                <button onClick={() => {
+                    props.changeFilter("completed")
+                }}>Completed
+                </button>
             </div>
         </div>
     )
