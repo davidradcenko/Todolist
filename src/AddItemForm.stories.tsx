beed2d5bc2f23d0c0e6bc24a88a65e0c1e48a9dd
David <davidradchenko2@gmail.com>
2022-06-17 19:34:35 +0300

    lessson 11 1 hour 5 min

---
+++ src/AddItemForm.stories.tsx
@@ -0,0 +1,12 @@
+import {AddItemForm} from "./AddItemForm";
+import React from "react";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'AddItemForm Component',
+    component : AddItemForm
+}
+const callback=action("Button 'add' was pressed inside  the form ")
+export  const  AddItemFormBaseExample = ()=>{
+    return <AddItemForm addItem={callback}/>
+}
---
+++ src/EditableSpan.stories.tsx
@@ -0,0 +1,13 @@
+import {AddItemForm} from "./AddItemForm";
+import React from "react";
+import {action} from "@storybook/addon-actions";
+import {EditableSpan} from "./EditableSpan";
+
+export default {
+    title: 'EditableSpan Component',
+    component : EditableSpan
+}
+const callback=action("Title changed ")
+export  const  EditableSpanBaseExample = ()=>{
+    return <EditableSpan title={"Start title"} onChange={callback}/>
+}
